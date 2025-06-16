53e0bbcb25f0a8746536edfbd388af2456bbe1c5
Reinaldo Andres Leal Brochero <rexleal360@gmail.com>
2025-06-16 15:52:59 -0500

    feat:the algoritmo was created

--- SPA.IMC/src/js/main.js
+++ SPA.IMC/src/js/main.js
@@ -1,1 +1,16 @@
-console.log("Hola ,Los Saludo desde js")
+let myWeight
+let myHeight
+let imc
+
+
+myHeight=prompt(message="please, write your height ")
+myWeight=prompt(message="please, write your Weight ")
+
+
+imc = myWeight / (myHeight * myHeight)
+
+alert("your IMC is " + imc)
+
+if (condition) {
+    
+}
